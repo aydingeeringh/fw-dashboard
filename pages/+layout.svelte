5d56fbffae14581af4011ac6eac703561c7b2cbb
aydingeeringh <aydin@geeringh.com>
2025-08-22 11:51:01 +0200

    update svelte layout

---
+++ pages/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+
+<EvidenceDefaultLayout {data} hideSidebar=true fullWidth=true hideHeader=true hideTOC=true neverShowQueries=true homePageName="Dashboard">
+	<slot slot="content" />
+</EvidenceDefaultLayout>
